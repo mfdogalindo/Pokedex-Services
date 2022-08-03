66d12947fae20d492833f4c5d7765f43a821729b
Manuel Galindo <mfdogalindo@outlook.com>
2022-08-03 08:53:41 -0500

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpExceptionFilter } from './Common/Exceptions/HttpExceptionFilter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
