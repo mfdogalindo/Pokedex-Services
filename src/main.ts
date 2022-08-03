f710420f7be0e65a37f83b3e93566f58b67fbe45
Manuel Galindo <mfdogalindo@outlook.com>
2022-08-03 08:33:48 -0500

    Port env

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { HttpExceptionFilter } from './Common/Exceptions/HttpExceptionFilter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
